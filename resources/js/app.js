004426598f1804db3d19e0e2b059f964eee523d0
Zakki Zuboys <idbla1003@gmail.com>
2022-03-12 11:34:47 +0700

    first

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,9 @@
 require('./bootstrap');
 
 require('./components/HelloReact')
+
+import ReactDOM from 'react-dom';
+
+if (document.getElementById('hello-react')) {
+    ReactDOM.render(<HelloReact />, document.getElementById('hello-react'));
+}
